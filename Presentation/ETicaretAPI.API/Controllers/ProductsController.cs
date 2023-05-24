9930a9bd5c226d74f65bca552ba1935066d1b27e
yasingul <yasngl46@gmail.com>
2023-05-24 19:45:51 +0300

    Ürün Ekleme İşlemleri Yapıldı- ProductService

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -13,24 +13,13 @@ namespace ETicaretAPI.API.Controllers       //Test işlemlerini burada hallediyo
     {
         private readonly IProductWriteRepository _productWriteRepository;
         private readonly IProductReadRepository _productReadRepository;
-
-        //readonly private IOrderWriteRepository _orderWriteRepository;
-        //readonly private IOrderReadRepository _orderReadRepository;
-
-        //readonly private ICustomerWriteRepository _customerWriteRepository;
         public ProductsController(
             IProductWriteRepository productWriteRepository,
             IProductReadRepository productReadRepository
-            //IOrderWriteRepository orderWriteRepository,
-            //ICustomerWriteRepository customerWriteRepository,
-            //IOrderReadRepository orderReadRepository
             )
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
-            //_orderWriteRepository = orderWriteRepository;
-            //_customerWriteRepository = customerWriteRepository;
-            //_orderReadRepository = orderReadRepository;
         }
         [HttpGet]
         public async Task<IActionResult> Get()
